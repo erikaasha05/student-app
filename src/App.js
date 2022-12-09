06b9b4d4a8d5f68b69f3dd5085ee2df5729fc554
Erika Sha <erikaasha05@gmail.com>
2022-12-08 22:21:50 -0500

    components

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ function App() {
   return (
     <main>
       <h1>Attendance</h1>
-      Here's the first rendered student list:
-      <StudentList></StudentList>
-      Here's the second rendered student list:
       <StudentList></StudentList>
     </main>
   );
