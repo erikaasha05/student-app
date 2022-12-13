2f7f77cfe64a3473295ccdaed2162783d8117d4a
Erika Sha <erikaasha05@gmail.com>
2022-12-12 23:04:09 -0500

    completed props

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "./StudentList.css";
+import Student from "./Student";
 
-const StudentList = () => {
+const StudentList = (props) => {
+  const studentComponents = props.students.map((student, index) => {
+    return (
+      <li key={index}><Student name={student.nameData} email={student.emailData}></Student></li>
+    );
+  });
+
   return (
     <section>
       <h2 className="student-list_heading">Student List</h2>
       <ul className="student-list">
-        <li>Student A</li>
-        <li>Student B</li>
-        <li>Student C</li>
+        {studentComponents}
       </ul>
     </section>
   );
