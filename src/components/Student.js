25b1eed079fbe57e315a6c4607288222653f0084
Erika Sha <erikaasha05@gmail.com>
2022-12-16 12:30:24 -0500

    Lifting State Up

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,18 +1,40 @@
 import "./Student.css";
 import PropTypes from "prop-types";
+// import { useState } from "react";
 
 const Student = (props) => {
+  const onAttendanceButtonClick = () => {
+    const updatedStudent = {
+      id: props.id,
+      nameData: props.name,
+      emailData: props.email,
+      isPresentData: !props.isPresent,
+    };
+
+    props.onUpdate(updatedStudent);
+  };
+
+  const nameColor = props.isPresent ? "green" : "red";
+
   return (
-    <ul>
-      <li>Nickname: {props.name}</li>
-      <li>Email: {props.email}</li>
-    </ul>
+    <div>
+      <ul>
+        <li className={nameColor}>Nickname: {props.name}</li>
+        <li>Email: {props.email}</li>
+      </ul>
+      <button onClick={onAttendanceButtonClick}>
+        Toggle if {props.name} is present
+      </button>
+    </div>
   );
 };
 
 Student.propTypes = {
+  id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   email: PropTypes.string.isRequired,
+  isPresent: PropTypes.bool,
+  onUpdate: PropTypes.func.isRequired,
 };
 
 export default Student;
